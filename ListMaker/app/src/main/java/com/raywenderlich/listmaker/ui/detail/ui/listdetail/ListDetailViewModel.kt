ff2ed9a977cb4d22d3d56964a961fbc9ec973e96
MalachiV12303 <malachivalle12303@gmail.com>
2025-05-22 02:52:10 -0400

    first commit

---
+++ ListMaker/app/src/main/java/com/raywenderlich/listmaker/ui/detail/ui/listdetail/ListDetailViewModel.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.listmaker.ui.detail.ui.listdetail
+import androidx.lifecycle.ViewModel
+import com.raywenderlich.listmaker.TaskList
+class ListDetailViewModel() : ViewModel() {
+    lateinit var onTaskAdded: (() -> Unit)
+    lateinit var list: TaskList
+    fun addTask(task: String) {
+        list.tasks.add(task)
+        onTaskAdded.invoke()
+    }
+}
